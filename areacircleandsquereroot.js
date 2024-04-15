f8aefc6dbe8eb851bcf44961428e78f31fd98793
“aya <ayahegab2k@gmail.com>
2024-04-15 02:58:49 +0200

    first commit

---
+++ areacircleandsquereroot.js
@@ -0,0 +1,16 @@
+var circleRadios = Number(prompt("Enter the value of a circle’s radius"));
+confirm(`Total area of the circle is ${Math.PI * Math.pow(circleRadios, 2)}`);
+
+var userNum = Number(prompt("what`s value to calculate its square root?"));
+confirm(`squar root of ${userNum} is ${Math.sqrt(userNum)}`);
+
+// var area = function (radios) {
+//   return Math.PI * Math.pow(radios, 2);
+// };
+
+// var calcSqRoot = function (value) {
+//   return Math.sqrt(value);
+// };
+
+// console.log(area(5));
+// console.log(calcSqRoot(16));
